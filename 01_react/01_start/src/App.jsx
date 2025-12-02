450f6b2acbd93657e447965471783ca7e0819b79
Mohammed Saleh <104917244+Mdsaleh99@users.noreply.github.com>
2025-12-02 19:07:13 +0530

    add: begin the course

---
+++ 01_react/01_start/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Learn react with Chaicode.com</h1>
+      <p>I would love to change this.</p>
+    </>
+  );
+}
+
+export default App;
